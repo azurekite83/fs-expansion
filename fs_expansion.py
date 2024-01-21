8f944a09e50b216bef9ebc81d7c06ed61ecb2472
azurekite83 <azurekite83@gmail.com>
2024-01-21 05:29:34 -0800

    Added distro detection

--- fs_expansion.py
+++ fs_expansion.py
@@ -1,8 +1,9 @@
-import sys, os, argparse
+import sys, os, argparse, csv
+from os import path
 
 #Use sfdisk to give commands to change partitions
 #Arguments:
-#   -lvm/physical partition
+#   -logical/physical partition
 #   -which partition
 #   -shrink or grow partition
 #   -backup before action
@@ -23,6 +24,36 @@ def parse_arguments(args):
     argument_list = parser.parse_args(args[1:])
     return argument_list
 
+def check_binary_exists(binary_required):
+    #see if sfdisk util exists
+    sub_paths = os.getenv("PATH").split(":")
+
+    for directory in sub_paths:
+        if path.exists(directory + binary_required):
+            return True
+
+    return False
+
+def install_binary(binary):
+   #Check which package manager is being used
+    """
+        Currently having to manually put in
+        what package managers distros use in
+        a csv file.
+        Will continue to be trash until I decide
+        this is worth being expanded upon.
+    """
+    with open("pkg-mng.csv", "r") as csv_file:
+        csv_parsed = csv.DictReader(csv_file)
+
+        for row in csv_parsed:
+            print(row)
+
+   #After package manager is detected run command
+def run(necessary_binaries, arguments):
+    print(necessary_binaries)
+    print(arguments)
+
 def main():
     uid = os.getuid()
 
@@ -30,7 +61,7 @@ def main():
     if uid != 0:
         print("This program needs elevated privileges, run as root.")
         exit(1)
-    
+    #gather arguments 
     argument_list = parse_arguments(sys.argv)
     
     partition = argument_list.partition
@@ -38,6 +69,13 @@ def main():
     partition_increase = argument_list.grow
     partition_decrease = argument_list.shrink
 
+    #check that necessary binaries exist
+    necessary_binaries = {"sfdisk": False, "lsblk": False}
 
+    for binary in necessary_binaries:
+        necessary_binaries[binary] = check_binary_exists(binary) 
+        if necessary_binaries[binary] == False:
+            install_binary(binary)
+    
 if __name__ == "__main__":
     main()

cd426e1d791411be343d73d80d30cd5f9be21bd4
azurekite83 <azurekite83@gmail.com>
2024-02-06 01:43:21 -0800

    Added: Unit tests, fixed bugs as needed

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -2,14 +2,110 @@ from helpers import parse_table, get_columns, fetch_element, check_for_space, ex
 import subprocess
 import pytest
 
-def test_execute_command_pass():
+#   *** def execute_command(): ***
+
+def test_pass_a():
     passing_command = "lsblk -b"
     
     assert execute_command(passing_command) == None
 
-def test_execute_command_no_whitespace():
+def test_invalid_command():
     failing_command = "jibberishsh"
 
     with pytest.raises(Exception):
         execute_command(failing_command)
+
+def test_no_whitespace():
+    failing_command = "lsblk-b"
+
+    with pytest.raises(Exception):
+        execute_command(failing_command)
+
+def test_empty():
+    with pytest.raises(Exception):
+        execute_command("")
+
+def test_invalid_syntax():
+    with pytest.raises(Exception):
+        execute_command("lsblk -listing")
+
+# *** def check_for_space(): ***
+
+def test_pass_b():
+    assert check_for_space(5000, "500M") == True
+
+def test_no_space():
+    assert check_for_space(50, "500M") == False
+
+def test_wrong_suffix():
+    assert check_for_space(5000, "500P") == False
+
+def test_case_pass():
+    cases = ["200G", "200g", "200K", "200k"]
+    results = []
+
+
+    for case in cases:
+        results.append(check_for_space("500000", case))
+
+    assert results == [True, True, True, True]
+
+def test_case_fail():
+    cases = ["200x", "200p", "200t", "200X", "200P", "200T"]
+    results = []
+
+    for case in cases:
+        results.append(check_for_space("500000", case))
+
+    assert results == [False, False, False, False, False, False]
+
+def test_invalid_size_increase():
+    with pytest.raises(Exception):
+        check_for_space("500000", "piglet")
+
+# *** def parse_table(): ***
+
+def test_pass_c():
+    row_test = ["NAME", "MAJ:MIN", "RM", "SIZE", "RO", "TYPE", "MOUNTPOINTS"]
+    table = parse_table("lsblk -l")
+
+    assert row_test in table 
+
+def test_command_fail(capsys):
+    with pytest.raises(SystemExit):
+        parse_table("lsbak -list")
+
+# *** def get_columns(): *** 
+
+def test_pass_d():
+    data = [["a", "b"], ["1 2", "3 4"]]
+
+    assert get_columns(data, [0]) == [["a"], ["1 2"]]
+
+def test_none_in_table():
+    data = [["a", "b", "c"], ["1 2", "3 4"]]
+
+    assert get_columns(data, [2]) == [["c"], [None]]
+
+
+# *** def fetch_element(): ***
+
+def test_pass_e():
+    data = [["a", "b", "c"],
+            ["1 2", "3 4", "5 6"]]
+
+    assert fetch_element(data, 0, 2, "1 2") == "5 6"
+
+def test_none_in_row():
+    data = [["a", "b", "c", "d"],
+            ["1 2", "3 4", "5 6"]]
+
+    assert fetch_element(data, 0, 3, "1 2") == None
+
+
+
+
+
+
+
 

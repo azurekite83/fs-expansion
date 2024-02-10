b4a0f13f545a9aa68fbac9b044bd67ef724860d5
azurekite83 <azurekite83@gmail.com>
2024-02-09 21:12:08 -0800

    Fixed: Refactored code, removed variables that were unnecessary

--- src/partitionaid/run.py
+++ src/partitionaid/run.py
@@ -1,28 +1,19 @@
-import re, subprocess
+import re
 from src.partitionaid.helpers import fetch_element, parse_table, get_columns, check_for_space, execute_command
+from src.partitionaid.operations import grow_physical_partition
 
-def grow_partition(partition, partition_increase):
-    print("Placeholder")
 
 def run_program(arguments):
-    #find what partitions mount point is
-    #Initially I thought that umount syntax
-    #required you to provide the mountpoint to unmount
+        
+    #TODO: -Make get_column functions use named fields and not magic numbers 
     
-    #I was mistaken. I don't feel like deleting this code though.
-    
-    #TODO: Add functionality to close any open processes if mount point is active
-    #      -Make get_column functions use named fields and not magic numbers
     lsblk_table = parse_table("lsblk -b --list -o +START,FSAVAIL")
-    df_table = parse_table("df -B 1M")
 
     lsblk_parsed = get_columns(lsblk_table, [0, 3, 5, 6, 7, 8])
-    df_parsed = get_columns(df_table, [0, 3])
 
     partition = arguments["partition"].removeprefix("/dev/")
 
     #To remember for when we remount
-    mountpoint_of_partition = fetch_element(lsblk_parsed, 0, 6, partition)
     type_of_partition = fetch_element(lsblk_parsed, 0, 5, partition)
 
     
@@ -32,90 +23,7 @@ def run_program(arguments):
     #Right now basic functionality only supports lvm and part
 
     if arguments["grow"] != None and type_of_partition == "part":
-        #Compare size increase to space available 
-        #Note: This is using unallocated space at the end of the device to move partitions,
-        #but this can also be done if one of the partitions has enough space as well
-
-        #TODO: Add functionality to search through partitions if there is not enough unallocated space
-        #       -Add regex generation depending on type of partition
-
-        #See where partitions placement is in filesystem
-        partition_number_regex = re.compile("[0-9]{1}")
-        device_regex = re.compile("sd[a-z]{1}")
-        size_increase_regex = re.compile("[0-9]+")
-
-        partition_number_found = partition_number_regex.search(partition)
-        device_found = device_regex.search(partition)
-        size_increase_found = size_increase_regex.match(arguments["grow"])
-
-        if partition_number_found == None or device_found == None:
-            print("Invalid argument for partition. Aborting...")
-            exit(1)
-        else:
-            partition_number_found = partition_number_found.group()
-            device_found = device_found.group()
-        
-        #check if there is enough space
-        available_space = (int(fetch_element(lsblk_parsed, 0, 8, device_found)) / (1024**2)) 
-        is_space_available = check_for_space(available_space, arguments["grow"]) 
-
-        if not is_space_available:
-            print("Not enough space for operation. Aborting...")
-            exit(1)
-        
-        
-        #Find highest partition number
-        highest_partition = None
-        
-        for row in lsblk_parsed:
-            if device_found in row[0]:
-                current_partition_number = partition_number_regex.search(row[0]).group()
-
-                if current_partition_number > partition_number_found:
-                    highest_partition = current_partition_number
-
-        for i in range(highest_partition, partition_number_found, -1):
-            current_partition = f"{device_found}{i}"
-            partition_start_sector = int(fetch_element(lsblk_parsed, 0, 7, current_partition)) / (1024 ** 2)
-            byte_suffix = arguments["grow"].removeprefix(size_increase_found)
-
-            if byte_suffix == "G" or byte_suffix == "g":
-                size_increase_found *= 1024 
-
-            #New partition start sector
-            new_partition_start = partition_start_sector + size_increase_found
-            #Size of partition in Mib
-            size_of_partition = (int(fetch_element(lsblk_parsed, 0, 3, current_partition)) / (1024 ** 2))
-            
-            if current_partition == partition:
-                total_size_increase = size_of_partition + size_increase_found
-                execute_command(f"umount -l {arguments['partition']}")
-                execute_command(f"sfdisk --delete {arguments['partition']}")
-                execute_command(f"echo -e 'size={total_size_increase}M start={partition_start_sector}M' | sfdisk /dev/{device_found}")
-            #TODO: Figure out remounting stuff
-            
-            #If --backup is included
-            if arguments["backup"] == True:
-                execute_command(f"sfdisk --dump {arguments['partition']} > part.dump") 
-
-            #Unmount partitions
-
-            #Using -l flag for lazy unmount just in case there are
-            #any processes still running on the partition
-            execute_command(f"umount -l /dev/{current_partition}")
-        
-            #Delete partition
-            execute_command(f"sfdisk --delete {current_partition}")
-            
-            #Create partition with adjusted start and end blocks
-            #TODO: Test on VM because I don't know what order partitions are created in
-            execute_command(f"echo -e 'size={size_of_partition}M, start={new_partition_start}M' | sfdisk /dev/{device_found}")
-            
-            #TODO: Remount partition if it was mounted
-            #      -Forgot that some partitions have flags and stuff, gotta check for those :)))))
-        
-        #resize partition to be increased
-        
+        grow_physical_partition(lsblk_parsed, arguments["partition"], arguments["grow"], arguments["backup"])
         
     elif type_of_partition == "lvm":
         #Do logical partition things

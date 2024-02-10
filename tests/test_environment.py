9af14c01e1ce4c92564d51df792cbc84b53bf4c4
azurekite83 <azurekite83@gmail.com>
2024-02-09 21:10:26 -0800

    Added: Test skipping in case tests are ran on windows system Fixed: Comparisons, wrong directory when testing for distribution (opens pkg-mng.csv in wrong dir)

--- tests/test_environment.py
+++ tests/test_environment.py
@@ -5,11 +5,11 @@ import pytest, sys
 @pytest.mark.skipif(sys.platform.startswith("win32") == True, reason="Not currently compatible with Windows.")
 def test_existing_binary():
     #Should always pass on most linux systems
-    assert check_binary_exists("ls") == True
+    assert check_binary_exists("ls") is True
 
 @pytest.mark.skipif(sys.platform.startswith("win32") == True, reason="Not currently compatible with Windows.")
 def test_non_existent_binary():
-    assert check_binary_exists("pig") == False
+    assert check_binary_exists("pig") is False
 
 
 # *** def fetch_commands(): ***
@@ -21,11 +21,11 @@ def test_distro_arch(monkeypatch):
     assert fetch_commands("arch") == ["pacman -S"]
 # *** def install_binary(): ***
 
-@pytest.mark.skipif(sys.platform.startswith("win32") == True, reason="Not currently compatible with Windows.")
+@pytest.mark.skip(reason="No known way of currently testing this.")
 def test_binary_install():
     #Very limited currently, if this fails on
     # your system, make sure to have included binaries needed.
     # Refer to __main__.py
 
-    assert install_binary("neofetch") == None
+    assert install_binary("neofetch") is None
 

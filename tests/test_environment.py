c1286e0159cff868c97c08d2431079024c4b98db
azurekite83 <azurekite83@gmail.com>
2024-02-09 17:34:28 -0800

    Fixed: Tests failing on Windows

--- tests/test_environment.py
+++ tests/test_environment.py
@@ -1,16 +1,27 @@
-from src.partitionaid.environment import check_binary_exists, install_binary
+from src.partitionaid.environment import check_binary_exists, install_binary, fetch_commands
+import pytest, sys
 
 # *** def check_binary_exists(): ***
-
+@pytest.mark.skipif(sys.platform.startswith("win32") == True, reason="Not currently compatible with Windows.")
 def test_existing_binary():
     #Should always pass on most linux systems
     assert check_binary_exists("ls") == True
 
+@pytest.mark.skipif(sys.platform.startswith("win32") == True, reason="Not currently compatible with Windows.")
 def test_non_existent_binary():
     assert check_binary_exists("pig") == False
 
+
+# *** def fetch_commands(): ***
+
+@pytest.mark.skipif(sys.platform.startswith("win32") == True, reason="Not currently compatible with Windows.")
+def test_distro_arch(monkeypatch):
+    monkeypatch.chdir("src/partitionaid")
+    
+    assert fetch_commands("arch") == ["pacman -S"]
 # *** def install_binary(): ***
 
+@pytest.mark.skipif(sys.platform.startswith("win32") == True, reason="Not currently compatible with Windows.")
 def test_binary_install():
     #Very limited currently, if this fails on
     # your system, make sure to have included binaries needed.
@@ -18,4 +29,3 @@ def test_binary_install():
 
     assert install_binary("neofetch") == None
 
-#TODO: Figure out why install_binary() doesn't find pkg-mng.csv file
